ea5df166dba849ce8ea8c717120f9f0592d1c37f
Przemysław Pitus <nexit07@gmail.com>
2023-11-26 21:54:33 +0100

    Allow to share link to room

--- frontend/src/pages/Room/RoomLobby/LobbyHeader.tsx
+++ frontend/src/pages/Room/RoomLobby/LobbyHeader.tsx
@@ -1,4 +1,5 @@
 import { RoomResponse } from '../../../api/useGetRoom';
+import { useEffect, useState } from 'react';
 
 interface Props {
   room: RoomResponse;
@@ -7,8 +8,37 @@ interface Props {
 export const LobbyHeader = (props: Props) => {
   const { room } = props;
 
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (copied)
+      window.setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+  }, [copied]);
+
   return <div className={`bg-gray-100 flex justify-between mx-8 p-4`}>
     <div>#{room.code}</div>
-    <div className={`text-right`}>Udostępnij link do pokoju</div>
+    <div
+      className={`text-right`}
+      onClick={() => {
+        const link = window.document.location.href;
+        const shareData = {
+          title: "Dołącz do pokoju w MamyKlocga",
+          url: link
+        }
+
+        if (!navigator.share || !navigator.canShare(shareData)) {
+          window.navigator.clipboard.writeText(link).then(() => {
+            setCopied(true);
+          });
+          return;
+        }
+
+        navigator.share(shareData).then() // Only over HTTPS
+      }}
+    >
+      {copied ? "Skopiowano link do pokoju!" : "Udostępnij link do pokoju"}
+    </div>
   </div>;
 };
